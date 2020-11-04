35940a2a579993e78b81a278b7c1f1fb49834808
Vinidel <vinicius.delascio@digio.com.au>
2020-11-05 09:36:16 +1100

    add heroku url to API

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-// export const BASE_API_URL = 'https://guarded-woodland-18511.herokuapp.com'
-export const BASE_API_URL = 'http://localhost:8080'
+export const BASE_API_URL = 'https://stormy-cliffs-52671.herokuapp.com/'
+// export const BASE_API_URL = 'http://localhost:8080'
