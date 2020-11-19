cd7ee6437c5d1383525a1da80fc6756313acce7d
Vinidel <vinicius.delascio@digio.com.au>
2020-11-19 16:33:40 +1100

    add Render prop pattern

---
+++ client/src/components/WithFetch.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from "axios";
+import useAuth0 from "@auth0/auth0-react/dist/use-auth0";
+
+const WithFetch = (props) => {
+  const { user } = useAuth0();
+  const [data, setData] = useState({});
+  const [isFetching, setIsFetching] = useState(true);
+
+  const fetch = () => {
+    axios.get(props.url,
+      {
+        headers: {
+          "x-user-id": user.sub
+        }
+      }).then((response) => {
+      setData(response.data);
+      setIsFetching(false);
+    })
+  }
+
+
+  useEffect(() => fetch(), []);
+
+  return props.render({data, isFetching})
+}
+
+WithFetch.propTypes = {
+  render: PropTypes.func.required,
+  url: PropTypes.string,
+}
